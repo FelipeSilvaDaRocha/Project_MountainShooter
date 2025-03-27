8ddd0bcfa35cad2d9c93ad038eb119562e6fc6ef
Felipe Rocha <felipesilvarocha01@gmail.com>
2025-03-26 23:51:28 -0300

    Creating Player Shot Creating Enemy Shot

--- code/Const.py
+++ code/Const.py
@@ -16,9 +16,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 1,
     'Player2': 3,
+    'Player2Shot': 2,
     'Enemy1': 2,
-    'Enemy2': 1
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2
 }
 
 ENTITY_HEALTH = {
@@ -44,6 +48,13 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
+}
+
 #  M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -60,7 +71,7 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
                    'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
                     'Player2': pygame.K_LCTRL}
 
 #  S
